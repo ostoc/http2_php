549407ab2940230a250501d3e5a0f744217a8336
Junyu Pu <pujunyu@gmail.com>
2016-03-18 19:02:18 +0100

    update proxy

--- curlHttp2.php
+++ curlHttp2.php
@@ -1,7 +1,10 @@
 <?php
-  // if(!define('CURL_HTTP_VERSION_2_0')){
-  //   define('CURL_HTTP_VERSION_2_0',3);
-  // }
+
+  # Use this script to test the cURL is working for HTTP2
+  # Need to notice, in order to use HTTP2:
+  # cURL >= 7.47.0
+  # Destination Server should enable http2
+  # PHP >= 5.5.24
 
   $ch = curl_init("https://http2.akamai.com/");
 
@@ -9,14 +12,26 @@
   curl_setopt($ch, CURLOPT_HTTP_VERSION,CURL_HTTP_VERSION_2_0);
   curl_setopt($ch, CURLOPT_HEADER, 1);
   curl_setopt($ch, CURLOPT_NOBODY, 1);
+  curl_setopt($ch, CURLOPT_VERBOSE, true);
+  $verbose = fopen('log', 'w+');
+  curl_setopt($ch, CURLOPT_STDERR, $verbose);
 
   //execute curl
-  curl_exec($ch);
+  $result = curl_exec($ch);
+
+  if ($result === FALSE) {
+        printf("cUrl error (#%d): %s<br>\n", curl_errno($ch),
+            htmlspecialchars(curl_error($ch)));
+      }
+
+  rewind($verbose);
+
+  $verboseLog = stream_get_contents($verbose);
+
+  // echo "Verbose information:\n<pre>", htmlspecialchars($verboseLog), "</pre>\n";
 
 
-  $result = curl_getinfo($ch);
-  echo $result;
+
 
   curl_close($ch)
-
 ?>
